782a7e50262d23acadd1db469be7511939e96ef5
20180217125 <2768364523@qq.com>
2020-11-30 21:15:17 +0800

    第二次提交

---
+++ src/main/java/com/example/demo/CorsDemoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsDemoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsDemoTestApplication.class, args);
+	}
+
+}
